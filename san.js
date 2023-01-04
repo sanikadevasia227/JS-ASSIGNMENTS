1ec5fe6e824817eb9062c379280711413189ac57
Devasia <sanika.devasia@capgemini.com>
2023-01-04 17:06:53 +0530

    commiting js changes

--- san.js
+++ san.js
@@ -1,3 +1,9 @@
 <script type="text/javascript">  
  alert("Hello Javatpoint");  
 </script>  
+<script>  
+var a=10;  
+var b=20;  
+var c=a+b;//It adds values of a and b variable  
+document.write(c);//prints sum of 10 and 20  
+</script>  
